c592f2e8700600b9bdd4a723abba1fc62259656f
martinsyx <martinsyx@gmail.com>
2018-01-29 17:00:36 +0800

    'add'

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
-
+app.set('views','./view');
+app.set('view engine','ejs');
+app.use(express.static('./public', {
+    maxAge: '0', //no cache
+    etag: true
+}));
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  // res.send('Hello World!');
+  res.render('index');
 });
 
 var server = app.listen(3000, function () {
